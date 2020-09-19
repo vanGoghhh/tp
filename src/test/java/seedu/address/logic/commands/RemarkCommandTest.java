33b165de2ad6b13708cae0725f5bd4e8d58cb16d
vanGoghhh <blackznigelng@gmail.com>
2020-09-19 22:12:23 +0800

    Add Remark Command

---
+++ src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
@@ -0,0 +1,13 @@
+package seedu.address.logic.commands;
+
+        import org.junit.jupiter.api.Test;
+
+        import static org.junit.jupiter.api.Assertions.*;
+
+class RemarkCommandTest {
+
+    @Test
+    void execute_addRemarkUnfilteredList_success() {
+
+    }
+}
