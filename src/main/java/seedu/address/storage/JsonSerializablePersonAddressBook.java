400b4e35f32324b01469e42ff00dfbdf2d929802
ameliatjy <e0425671@u.nus.edu>
2020-10-03 23:43:21 +0800

    Rename file

--- src/main/java/seedu/address/storage/JsonSerializablePersonAddressBook.java
+++ src/main/java/seedu/address/storage/JsonSerializablePersonAddressBook.java
@@ -14,34 +14,34 @@ import seedu.address.model.ReadOnlyPersonAddressBook;
 import seedu.address.model.information.Person;
 
 /**
- * An Immutable AddressBook that is serializable to JSON format.
+ * An Immutable PersonAddressBook that is serializable to JSON format.
  */
 @JsonRootName(value = "addressbook")
-class JsonSerializableAddressBook {
+class JsonSerializablePersonAddressBook {
 
     public static final String MESSAGE_DUPLICATE_PERSON = "Persons list contains duplicate person(s).";
 
     private final List<JsonAdaptedPerson> persons = new ArrayList<>();
 
     /**
-     * Constructs a {@code JsonSerializableAddressBook} with the given persons.
+     * Constructs a {@code JsonSerializablePersonAddressBook} with the given persons.
      */
     @JsonCreator
-    public JsonSerializableAddressBook(@JsonProperty("persons") List<JsonAdaptedPerson> persons) {
+    public JsonSerializablePersonAddressBook(@JsonProperty("persons") List<JsonAdaptedPerson> persons) {
         this.persons.addAll(persons);
     }
 
     /**
      * Converts a given {@code ReadOnlyPersonAddressBook} into this class for Jackson use.
      *
-     * @param source future changes to this will not affect the created {@code JsonSerializableAddressBook}.
+     * @param source future changes to this will not affect the created {@code JsonSerializablePersonAddressBook}.
      */
-    public JsonSerializableAddressBook(ReadOnlyPersonAddressBook source) {
+    public JsonSerializablePersonAddressBook(ReadOnlyPersonAddressBook source) {
         persons.addAll(source.getPersonList().stream().map(JsonAdaptedPerson::new).collect(Collectors.toList()));
     }
 
     /**
-     * Converts this address book into the model's {@code AddressBook} object.
+     * Converts this address book into the model's {@code PersonAddressBook} object.
      *
      * @throws IllegalValueException if there were any data constraints violated.
      */
