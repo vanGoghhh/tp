53c01d7cc2367dfa0d75087b4870a7c57d3ea3fd
ameliatjy <e0425671@u.nus.edu>
2020-10-04 00:09:08 +0800

    Rename file

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -17,7 +17,7 @@ import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.PersonAddressBook;
-import seedu.address.model.information.NameContainsKeywordsPredicate;
+import seedu.address.model.information.PersonNameContainsKeywordsPredicate;
 import seedu.address.model.information.Person;
 import seedu.address.testutil.EditPersonDescriptorBuilder;
 
@@ -120,7 +120,7 @@ public class CommandTestUtil {
 
         Person person = model.getFilteredPersonList().get(targetIndex.getZeroBased());
         final String[] splitName = person.getName().fullName.split("\\s+");
-        model.updateFilteredPersonList(new NameContainsKeywordsPredicate(Arrays.asList(splitName[0])));
+        model.updateFilteredPersonList(new PersonNameContainsKeywordsPredicate(Arrays.asList(splitName[0])));
 
         assertEquals(1, model.getFilteredPersonList().size());
     }
