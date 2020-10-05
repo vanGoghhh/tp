aa48b29156305d0c44dccdc4e491b5cd76ecc495
BreadPeanutButter <joonjie98@gmail.com>
2020-10-05 17:32:37 +0800

    Fix jobaddressbook.json not saving bug
    Originally, storage was not saving the jobadressbook.json file so changes were not saved when commands were executed. Added a line to save the jobaddressbook during command execution in LogicManager.

--- src/main/java/seedu/address/logic/LogicManager.java
+++ src/main/java/seedu/address/logic/LogicManager.java
@@ -47,6 +47,7 @@ public class LogicManager implements Logic {
         commandResult = command.execute(model);
 
         try {
+
             storage.savePersonAddressBook(model.getPersonAddressBook());
         } catch (IOException ioe) {
             throw new CommandException(FILE_OPS_ERROR_MESSAGE + ioe, ioe);
