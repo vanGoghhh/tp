57275ad2e9f60302cddb8b6fa0a3b1d42045fac1
BreadPeanutButter <joonjie98@gmail.com>
2020-10-11 14:01:31 +0800

    Create utility class for EditJobDescriptor

---
+++ src/test/java/seedu/address/testutil/EditJobDescriptorBuilder.java
@@ -0,0 +1,99 @@
+package seedu.address.testutil;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import seedu.address.logic.commands.EditJobCommand;
+import seedu.address.logic.commands.EditJobCommand.EditJobDescriptor;
+import seedu.address.model.information.Address;
+import seedu.address.model.information.Email;
+import seedu.address.model.information.Job;
+import seedu.address.model.information.Name;
+import seedu.address.model.information.Phone;
+import seedu.address.model.information.Priority;
+import seedu.address.model.tag.Tag;
+
+/**
+ * A utility class to help with building EditJobDescriptor objects.
+ */
+public class EditJobDescriptorBuilder {
+
+    private EditJobCommand.EditJobDescriptor descriptor;
+
+    public EditJobDescriptorBuilder() {
+        descriptor = new EditJobDescriptor();
+    }
+
+    public EditJobDescriptorBuilder(EditJobDescriptor descriptor) {
+        this.descriptor = new EditJobDescriptor(descriptor);
+    }
+
+    /**
+     * Returns an {@code EditJobDescriptor} with fields containing {@code job}'s details
+     */
+    public EditJobDescriptorBuilder(Job job) {
+        descriptor = new EditJobDescriptor();
+        descriptor.setJobTitle(job.getJobTitle());
+        descriptor.setCompanyName(job.getCompanyName());
+        descriptor.setPhone(job.getPhone());
+        descriptor.setEmail(job.getEmail());
+        descriptor.setAddress(job.getAddress());
+        descriptor.setPriority(job.getPriority());
+        descriptor.setTags(job.getTags());
+    }
+
+    /**
+     * Sets the {@code JobTitle} of the {@code EditJobDescriptor} that we are building.
+     */
+    public EditJobDescriptorBuilder withJobTitle(String jobTitle) {
+        descriptor.setJobTitle(new Name(jobTitle));
+        return this;
+    }
+
+    /**
+     * Sets the {@code Phone} of the {@code EditJobDescriptor} that we are building.
+     */
+    public EditJobDescriptorBuilder withPhone(String phone) {
+        descriptor.setPhone(new Phone(phone));
+        return this;
+    }
+
+    /**
+     * Sets the {@code Email} of the {@code EditJobDescriptor} that we are building.
+     */
+    public EditJobDescriptorBuilder withEmail(String email) {
+        descriptor.setEmail(new Email(email));
+        return this;
+    }
+
+    /**
+     * Sets the {@code Address} of the {@code EditJobDescriptor} that we are building.
+     */
+    public EditJobDescriptorBuilder withAddress(String address) {
+        descriptor.setAddress(new Address(address));
+        return this;
+    }
+
+    /**
+     * Sets the {@code Priority} of the {@code EditJobDescriptor} that we are building.
+     */
+    public EditJobDescriptorBuilder withPriority(String priority) {
+        descriptor.setPriority(new Priority(priority));
+        return this;
+    }
+
+    /**
+     * Parses the {@code tags} into a {@code Set<Tag>} and set it to the {@code EditJobDescriptor}
+     * that we are building.
+     */
+    public EditJobDescriptorBuilder withTags(String... tags) {
+        Set<Tag> tagSet = Stream.of(tags).map(Tag::new).collect(Collectors.toSet());
+        descriptor.setTags(tagSet);
+        return this;
+    }
+
+    public EditJobDescriptor build() {
+        return descriptor;
+    }
+}
