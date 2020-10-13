fd925c5289c642bf16911d45aaecb3402f779248
BreadPeanutButter <joonjie98@gmail.com>
2020-10-13 23:33:21 +0800

    Add test for Experience
    Create ExperienceTest class

---
+++ src/test/java/seedu/address/model/information/ExerienceTest.java
@@ -0,0 +1,42 @@
+package seedu.address.model.information;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+public class ExerienceTest {
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new Experience(null));
+    }
+
+    @Test
+    public void constructor_invalidExperience_throwsIllegalArgumentException() {
+        String emptyExperience = "";
+        String negativeExperience = "-1";
+        String wordExperience = "Twenty";
+        assertThrows(IllegalArgumentException.class, () -> new Experience(emptyExperience));
+        assertThrows(IllegalArgumentException.class, () -> new Experience(negativeExperience));
+        assertThrows(IllegalArgumentException.class, () -> new Experience(wordExperience));
+    }
+
+    @Test
+    public void isValidExperience() {
+        // null phone number
+        assertThrows(NullPointerException.class, () -> Experience.isValidExperience(null));
+
+        // invalid phone numbers
+        assertFalse(Experience.isValidExperience("")); // empty string
+        assertFalse(Experience.isValidExperience("  ")); // spaces only
+        assertFalse(Experience.isValidExperience("phone")); // non-numeric
+        assertFalse(Experience.isValidExperience("9011p041")); // alphabets within digits
+        assertFalse(Experience.isValidExperience("-100")); // negative number
+
+        // valid phone numbers
+        assertTrue(Experience.isValidExperience("0")); // zero
+        assertTrue(Experience.isValidExperience("9")); // positive
+        assertTrue(Experience.isValidExperience("  8  ")); // positive with whitespace
+    }
+}
