f81d6c5abff647e8fce0f8144b57fb309c9d2960
ameliatjy <e0425671@u.nus.edu>
2020-10-19 21:31:29 +0800

    Add JobPhoneContainsKeywordsPredicate

---
+++ src/main/java/seedu/address/model/information/predicate/JobPhoneContainsKeywordsPredicate.java
@@ -0,0 +1,32 @@
+package seedu.address.model.information.predicate;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import seedu.address.commons.util.StringUtil;
+import seedu.address.model.information.Job;
+
+/**
+ * Tests that a {@code Job}'s {@code Phone} matches all of the keywords given.
+ */
+public class JobPhoneContainsKeywordsPredicate implements Predicate<Job> {
+    private final List<String> keywords;
+
+    public JobPhoneContainsKeywordsPredicate(List<String> keywords) {
+        this.keywords = keywords;
+    }
+
+    @Override
+    public boolean test(Job job) {
+        return keywords.stream()
+                .allMatch(keyword -> StringUtil.containsPhraseIgnoreCase(job.getPhone().value, keyword));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof JobPhoneContainsKeywordsPredicate // instanceof handles nulls
+                && keywords.equals(((JobPhoneContainsKeywordsPredicate) other).keywords)); // state check
+    }
+
+}
