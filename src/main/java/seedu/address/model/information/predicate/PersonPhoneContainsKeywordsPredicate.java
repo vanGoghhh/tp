9ed09687983564541402ed808c29281c778d405e
ameliatjy <e0425671@u.nus.edu>
2020-10-19 21:28:17 +0800

    Add PersonPhoneContainsKeywordsPredicate

---
+++ src/main/java/seedu/address/model/information/predicate/PersonPhoneContainsKeywordsPredicate.java
@@ -0,0 +1,32 @@
+package seedu.address.model.information.predicate;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import seedu.address.commons.util.StringUtil;
+import seedu.address.model.information.Person;
+
+/**
+ * Tests that a {@code Person}'s {@code Phone} matches all of the keywords given.
+ */
+public class PersonPhoneContainsKeywordsPredicate implements Predicate<Person> {
+    private final List<String> keywords;
+
+    public PersonPhoneContainsKeywordsPredicate(List<String> keywords) {
+        this.keywords = keywords;
+    }
+
+    @Override
+    public boolean test(Person person) {
+        return keywords.stream()
+                .allMatch(keyword -> StringUtil.containsPhraseIgnoreCase(person.getPhone().value, keyword));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof PersonPhoneContainsKeywordsPredicate // instanceof handles nulls
+                && keywords.equals(((PersonPhoneContainsKeywordsPredicate) other).keywords)); // state check
+    }
+
+}
