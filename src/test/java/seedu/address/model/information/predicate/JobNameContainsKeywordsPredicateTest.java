4f1e4faed80f0bdf6b5fd236dc5abf4c3443923c
ameliatjy <e0425671@u.nus.edu>
2020-10-19 20:28:25 +0800

    Create new package

--- src/test/java/seedu/address/model/information/predicate/JobNameContainsKeywordsPredicateTest.java
+++ src/test/java/seedu/address/model/information/predicate/JobNameContainsKeywordsPredicateTest.java
@@ -1,4 +1,4 @@
-package seedu.address.model.information;
+package seedu.address.model.information.predicate;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
