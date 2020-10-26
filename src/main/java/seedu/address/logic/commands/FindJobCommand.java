9a11d3cf84202bcf023dbc10343efae58d554bee
ameliatjy <e0425671@u.nus.edu>
2020-10-26 10:02:18 +0800

    Modify FindJobCommand

--- src/main/java/seedu/address/logic/commands/FindJobCommand.java
+++ src/main/java/seedu/address/logic/commands/FindJobCommand.java
@@ -2,6 +2,8 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Predicate;
 
 import seedu.address.commons.core.Messages;
@@ -18,18 +20,42 @@ public class FindJobCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all jobs whose details contain all of "
             + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
             + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
-            + "Example: " + COMMAND_WORD + " n/samsung e/recruitment@samsung.com";
+            + "Example: " + COMMAND_WORD + " c/samsung e/recruitment@samsung.com";
 
-    private final Predicate<Job> predicate;
+    private final List<Predicate<Job>> predicates;
 
     public FindJobCommand(Predicate<Job> predicate) {
-        this.predicate = predicate;
+        List<Predicate<Job>> predicateList = new ArrayList<>();
+        predicateList.add(predicate);
+        this.predicates = predicateList;
+    }
+
+    public FindJobCommand(List<Predicate<Job>> predicates) {
+        this.predicates = predicates;
+    }
+
+    /**
+     * Returns a composed predicate that represents a short-circuiting logical AND of all predicates in the list
+     * {@code predicates}.
+     */
+    public static Predicate<Job> composePredicatesList(List<Predicate<Job>> predicates) {
+
+        if (predicates.size() == 1) {
+            return predicates.get(0);
+        }
+
+        Predicate<Job> composedPredicate = unused -> true;
+        for (Predicate<Job> p : predicates) {
+            composedPredicate = composedPredicate.and(p);
+        }
+
+        return composedPredicate;
     }
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        model.updateFilteredJobList(predicate);
+        model.updateFilteredJobList(composePredicatesList(predicates));
         return new CommandResult(
                 String.format(Messages.MESSAGE_JOBS_LISTED_OVERVIEW, model.getFilteredJobList().size()));
     }
@@ -38,6 +64,6 @@ public class FindJobCommand extends Command {
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof FindJobCommand // instanceof handles nulls
-                && predicate.equals(((FindJobCommand) other).predicate)); // state check
+                && predicates.equals(((FindJobCommand) other).predicates)); // state check
     }
 }
