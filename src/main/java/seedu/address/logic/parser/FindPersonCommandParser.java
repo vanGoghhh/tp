04aa27aad56666b84aef08520ca45ab8214a598d
ameliatjy <e0425671@u.nus.edu>
2020-10-29 15:59:41 +0800

    Update FindPersonCommandParser

--- src/main/java/seedu/address/logic/parser/FindPersonCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindPersonCommandParser.java
@@ -61,52 +61,62 @@ public class FindPersonCommandParser implements Parser<FindPersonCommand> {
         }
 
         if (arePrefixesPresent(argMultimap, PREFIX_NAME)) {
-            String name = argMultimap.getValue(PREFIX_NAME).get();
+            String name = argMultimap.getValue(PREFIX_NAME).orElse("");
+            checkInput(name);
             List<String> words = splitInput(name);
             predicates.add(new PersonNameContainsKeywordsPredicate(words));
         }
         if (arePrefixesPresent(argMultimap, PREFIX_PHONE)) {
-            String phone = argMultimap.getValue(PREFIX_PHONE).get();
+            String phone = argMultimap.getValue(PREFIX_PHONE).orElse("");
+            checkInput(phone);
             List<String> words = splitInput(phone);
             predicates.add(new PersonPhoneContainsKeywordsPredicate(words));
         }
         if (arePrefixesPresent(argMultimap, PREFIX_EMAIL)) {
-            String email = argMultimap.getValue(PREFIX_EMAIL).get();
+            String email = argMultimap.getValue(PREFIX_EMAIL).orElse("");
+            checkInput(email);
             List<String> words = splitInput(email);
             predicates.add(new PersonEmailContainsKeywordsPredicate(words));
         }
         if (arePrefixesPresent(argMultimap, PREFIX_ADDRESS)) {
-            String address = argMultimap.getValue(PREFIX_ADDRESS).get();
+            String address = argMultimap.getValue(PREFIX_ADDRESS).orElse("");
+            checkInput(address);
             List<String> words = splitInput(address);
             predicates.add(new PersonAddressContainsKeywordsPredicate(words));
         }
         if (arePrefixesPresent(argMultimap, PREFIX_EXPERIENCE)) {
-            String experience = argMultimap.getValue(PREFIX_EXPERIENCE).get();
+            String experience = argMultimap.getValue(PREFIX_EXPERIENCE).orElse("");
+            checkInput(experience);
             List<String> words = splitInput(experience);
             predicates.add(new PersonExperienceContainsKeywordsPredicate(words));
         }
         if (arePrefixesPresent(argMultimap, PREFIX_DATE_OF_APPLICATION)) {
-            String dateOfApplication = argMultimap.getValue(PREFIX_DATE_OF_APPLICATION).get();
+            String dateOfApplication = argMultimap.getValue(PREFIX_DATE_OF_APPLICATION).orElse("");
+            checkInput(dateOfApplication);
             List<String> words = splitInput(dateOfApplication);
             predicates.add(new PersonApplicationContainsKeywordsPredicate(words));
         }
         if (arePrefixesPresent(argMultimap, PREFIX_SALARY)) {
-            String salary = argMultimap.getValue(PREFIX_SALARY).get();
+            String salary = argMultimap.getValue(PREFIX_SALARY).orElse("");
+            checkInput(salary);
             List<String> words = splitInput(salary);
             predicates.add(new PersonSalaryContainsKeywordsPredicate(words));
         }
         if (arePrefixesPresent(argMultimap, PREFIX_BLACKLIST)) {
-            String blacklist = argMultimap.getValue(PREFIX_BLACKLIST).get();
+            String blacklist = argMultimap.getValue(PREFIX_BLACKLIST).orElse("");
+            checkInput(blacklist);
             List<String> words = splitInput(blacklist);
             predicates.add(new PersonBlacklistContainsKeywordsPredicate(words));
         }
         if (arePrefixesPresent(argMultimap, PREFIX_URL_LINK)) {
-            String url = argMultimap.getValue(PREFIX_URL_LINK).get();
+            String url = argMultimap.getValue(PREFIX_URL_LINK).orElse("");
+            checkInput(url);
             List<String> words = splitInput(url);
             predicates.add(new PersonUrlLinkContainsKeywordsPredicate(words));
         }
         if (arePrefixesPresent(argMultimap, PREFIX_TAG)) {
-            String tag = argMultimap.getValue(PREFIX_TAG).get();
+            String tag = argMultimap.getValue(PREFIX_TAG).orElse("");
+            checkInput(tag);
             List<String> words = splitInput(tag);
             predicates.add(new PersonTagsContainKeywordsPredicate(words));
         }
@@ -132,6 +142,18 @@ public class FindPersonCommandParser implements Parser<FindPersonCommand> {
         String[] words = userInput.split("\\s+");
         Collections.addAll(keywords, words);
         return keywords;
+    }
+
+    /**
+     * Checks if userInput is non-empty.
+     * @param userInput User specified keyword for a field.
+     * @throws ParseException if the user input does not conform the expected format.
+     */
+    public void checkInput(String userInput) throws ParseException {
+        if (userInput.length() == 0) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindPersonCommand.MESSAGE_USAGE));
+        }
     }
 
 }
