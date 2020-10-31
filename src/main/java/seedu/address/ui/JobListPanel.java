d972f20fd5281b927184450fda9eb628ad4863fd
vanGoghhh <blackznigelng@gmail.com>
2020-10-31 18:13:30 +0800

    Updated UI implemented switch tab command

--- src/main/java/seedu/address/ui/JobListPanel.java
+++ src/main/java/seedu/address/ui/JobListPanel.java
@@ -15,22 +15,30 @@ public class JobListPanel extends UiPart<Region> {
     private static final String FXML = "JobListPanel.fxml";
     private final Logger logger = LogsCenter.getLogger(PersonListPanel.class);
 
+    private MainWindow mainWindow;
+
     @FXML
     private ListView<Job> jobListView;
 
     /**
      * Creates a {@code PersonListPanel} with the given {@code ObservableList}.
      */
-    public JobListPanel(ObservableList<Job> jobList) {
+    public JobListPanel(ObservableList<Job> jobList, MainWindow mainWindow) {
         super(FXML);
         jobListView.setItems(jobList);
-        jobListView.setCellFactory(listView -> new JobListPanel.JobListViewCell());
+        jobListView.setCellFactory(listView -> new JobListPanel.JobListViewCell(mainWindow));
     }
 
     /**
      * Custom {@code ListCell} that displays the graphics of a {@code Person} using a {@code PersonCard}.
      */
     class JobListViewCell extends ListCell<Job> {
+
+        private MainWindow mainWindow;
+
+        public JobListViewCell(MainWindow mainWindow) {
+            this.mainWindow = mainWindow;
+        }
         @Override
         protected void updateItem(Job job, boolean empty) {
             super.updateItem(job, empty);
@@ -40,6 +48,7 @@ public class JobListPanel extends UiPart<Region> {
                 setText(null);
             } else {
                 setGraphic(new JobCard(job, getIndex() + 1).getRoot());
+                setOnMouseClicked(event -> mainWindow.updateDetailedJobPanel(job));
             }
         }
     }
