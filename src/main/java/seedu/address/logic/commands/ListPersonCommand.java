d2abb0f5ea5e5012d9d178d159a9463b98a2f4b1
vanGoghhh <blackznigelng@gmail.com>
2020-11-05 15:49:52 +0800

    Remove unused import

--- src/main/java/seedu/address/logic/commands/ListPersonCommand.java
+++ src/main/java/seedu/address/logic/commands/ListPersonCommand.java
@@ -3,7 +3,6 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
-import seedu.address.logic.parser.Parser;
 import seedu.address.model.Model;
 import seedu.address.model.information.Person;
 
