feab200718ab7e6850c644a3364a3c77c689b3ad
vanGoghhh <blackznigelng@gmail.com>
2020-11-08 16:41:09 +0800

    Edit UI window

--- src/main/java/seedu/address/commons/core/GuiSettings.java
+++ src/main/java/seedu/address/commons/core/GuiSettings.java
@@ -11,7 +11,7 @@ import java.util.Objects;
 public class GuiSettings implements Serializable {
 
     private static final double DEFAULT_HEIGHT = 700;
-    private static final double DEFAULT_WIDTH = 1280;
+    private static final double DEFAULT_WIDTH = 1300;
 
     private final double windowWidth;
     private final double windowHeight;
